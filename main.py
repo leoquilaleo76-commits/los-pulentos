b5fcfa5a550905df1f495594475b4e5f08490c0c
leoquilaleo76 <leoquilaleo76@gmail.com>
2026-09-17 11:23:42 -0300

    .

---
+++ main.py
@@ -0,0 +1,10 @@
+from paciente import Paciente
+
+def main():
+    #crando un objeto paciente
+    p1=Paciente("12345678-9","Juan Perez",30,"Fonasa")
+    print(p1)
+    
+    
+if __name__=="__main__":
+    main()
--- paciente.py
+++ paciente.py
@@ -1,4 +1,6 @@
 class Paciente:
+    PREVIONES_VALIDAS:set[str]={"Fonasa","Isapre","Particular","Otro"}  
+   
     def __init__(self,rut:str, nombre:str, edad:int,previson:str):
         self.rut=rut
         self.nombre=nombre
@@ -13,3 +15,35 @@ class Paciente:
     @rut.setter
     def rut(self,rut:str)-> None:
         self.__rut=rut
+        @property
+        def nombre(self)-> str:
+            return self.__nombre
+
+        @nombre.setter
+        def nombre(self,nombre:str)-> None:
+            self.__nombre=nombre
+            @property
+            def edad(self)-> int:
+                return self.__edad
+            
+            
+            @edad.setter
+            def edad(self,edad:int)-> None:
+                self.__edad=edad
+                
+                @property
+                def previson(self)-> str:
+                    return self.__previson
+                
+                
+                @previson.setter
+                def previson(self,previson:str)-> None:
+                    self.__previson=previson
+                    
+                    def __str__(self)-> str:
+                        return f"información del paciente: {self.rut} - {self.nombre} - {self.edad} - {self.previson}"
+                    
+                    def __repr__(self)-> str:
+                        return f"Paciente({self.rut},{self.nombre},{self.edad},previson='{self.previson}')"
+                    
+                    
